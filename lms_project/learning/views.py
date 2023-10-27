9efdaca798df70c1b6ff1ca8b02267963c187bff
PyCharmer12 <ruby.royd.99@gmail.com>
2023-10-27 16:05:56 +0400

    Урок-5.

--- lms_project/learning/views.py
+++ lms_project/learning/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Course
+from datetime import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Здесь будет список всех доступных курсов')
+    courses = Course.objects.all()
+    current_year = datetime.now().year
+    return render(request, context={'courses': courses, 'current_year': current_year},
+                  template_name='index.html')
 def create(request):
     return HttpResponse('Здесь будет форма для создания собственного курса')
 def delete(request, course_id):
@@ -12,3 +17,4 @@ def detail(request, course_id):
     return HttpResponse(f'Здесь мы узнаем детальную информацию о {course_id} курсе')
 def enroll(request, course_id):
     return HttpResponse(f'Здесь мы сможем записаться на {course_id} курс')
+
