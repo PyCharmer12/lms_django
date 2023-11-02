d8da3712bc9b80b01f13bf0ec37d3300b30fda11
PyCharmer12 <ruby.royd.99@gmail.com>
2023-11-02 15:29:47 +0400

    Урок-6.

--- lms_project/auth_app/views.py
+++ lms_project/auth_app/views.py
@@ -1,14 +1,31 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
+# Здесь реализован обработчик введенных пользователем
+# логина и пароля при попытке залогиниться на сайт:
 def login(request):
-    return HttpResponse('Эта страница для входа пользователя на <b>сайт</b>')
+    if request.method == 'POST':
+        data = request.POST
+        user = authenticate(email=data['email'], password=data['password'])
+        if user and user.is_active:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Ваш аккаунт заблокирован')
+    else:
+            return render(request, 'login.html')
+
+
 def register(request):
     return HttpResponse('Эта страница для регистрации пользователя')
+
 def logout(request):
-    return HttpResponse('Это представление выполняет выход и редирект на страницу входа')
+    logout(request)
+    return redirect('login')
+
 def change_password(request):
     return HttpResponse('Этот обработчик меняет пароль пользователя')
 def reset_password(request):
--- lms_project/learning/views.py
+++ lms_project/learning/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Course
+from django.shortcuts import render, redirect
+from .models import *
 from datetime import *
 
 # Create your views here.
@@ -9,12 +9,51 @@ def index(request):
     current_year = datetime.now().year
     return render(request, context={'courses': courses, 'current_year': current_year},
                   template_name='index.html')
+
+# Обработчик запроса создания курса :
 def create(request):
-    return HttpResponse('Здесь будет форма для создания собственного курса')
+    if request.method == 'POST':
+        data = request.POST
+        Course.objects.create(title=data['title'], author=request.user,
+                              description=data['description'], start_date=data['start_date'],
+                              duration=data['duration'], price=data['price'],
+                              count_lessons=data['count_lessons'])
+        return redirect('index')
+    else:
+        return render(request, 'create.html')
+
+
+
+# Обработчик запроса удаления курса :
 def delete(request, course_id):
-    return HttpResponse(f'Здесь будет производиться удаление {course_id} курса')
+    Course.objects.get(id=course_id).delete()
+    return render('index')
+
+
+# Обработчик запроса вывода подробной информации о курсе:
 def detail(request, course_id):
-    return HttpResponse(f'Здесь мы узнаем детальную информацию о {course_id} курсе')
+    course = Course.objects.get(id=course_id)
+    lessons = Lesson.objects.filter(course=course_id)
+    context = {'course': course, 'lessons': lessons}
+    return render(request,'detail.html', context)
+
+# Обработчик запроса записи на курс:
 def enroll(request, course_id):
-    return HttpResponse(f'Здесь мы сможем записаться на {course_id} курс')
+    # Проверка, авторизован ли пользователь:
+    if request.user.is_anonymous: # Если не авторизован,
+        return redirect('login') # Перенаправить на страницу входа
+    else: # Иначе, если пользователь авторизован, то:
+        # Проверка - пользователь уже записан на курс?
+        is_existed = Tracking.objects.filter(user=request.user).exists()
+        if is_existed: # Если записан, то вывод уведомления:
+            return HttpResponse(f'Вы уже записаны на данный курс')
+        else: # Если не записан, то отобразить уроки конкретного курса:
+            lessons = Lesson.objects.filter(course=course_id)
+            # И создать набор экземпляров модели Tracking с атрибутами, указанным в models.py,
+            records = [Tracking(lesson=lesson, user=request.user, possed=False) for lesson in lessons]
+            # Затем создать набор записей в таблице на основе созданного списка records:
+            Tracking.objects.bulk_create(records)
+            # Выводим уведомление об успешной записи на курс:
+            return HttpResponse('Вы записаны на данный курс')
+
 
